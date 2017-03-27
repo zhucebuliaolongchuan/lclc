5d4934381fc2ae14d85f231ec2583fd8ace986a6
zhucebuliaolongchuan <cl4076@nyu.edu>
2017-03-26 22:42:22 -0400

    updated myLeetcode

--- updateLC.sh
+++
@@ -1,6 +0,0 @@
-git remote remove lclc
-git remote add lclc https://github.com/zhucebuliaolongchuan/lclc.git
-git pull lclc master --allow-unrelated-histories
-git add *
-git commit -m "updated myLeetcode"
-git push lclc master
