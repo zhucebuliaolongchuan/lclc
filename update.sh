7e403a8337f313f9c25cf2ad996015de8ac77f48
zhucebuliaolongchuan <cl4076@nyu.edu>
2017-03-26 23:01:06 -0400

    updated myLeetcode on 'date + %d%H%M'

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ git remote remove lclc
 git remote add lclc https://github.com/zhucebuliaolongchuan/lclc.git
 git pull lclc master --allow-unrelated-histories
 git add *
-git commit -m "updated myLeetcode on $time_spot"
+git commit -m "updated myLeetcode on 'date + %d%H%M'"
 git push lclc master
