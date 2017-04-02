585c0eb481aaee48dd7d1cee995ddcb5405afc7a
zhucebuliaolongchuan <cl4076@nyu.edu>
2017-04-02 19:22:13 -0400

    updated on 2017-04-02 19:22:13

--- Backtracking/LC320_GeneralizedAbbreviation.py
+++ Backtracking/LC320_GeneralizedAbbreviation.py
@@ -2,6 +2,7 @@
 320. Generalized Abbreviation
 Write a function to generate the generalized abbreviations of a word.
 """
+
 class Solution(object):
     def generateAbbreviations(self, word):
         """
@@ -10,16 +11,13 @@ class Solution(object):
         """
         res = []
         self.backtrack(word, 0, '', 0, res)
-        return res
-
+        return re
+s
     def backtrack(self, word, pos, cur, count, res):
+	str_count = '' if count == 0 else str(count)
         if pos == len(word):
-            if count > 0:
-                cur += str(count)
+	    cur += str_count
             res.append(cur)
         else:
             self.backtrack(word, pos + 1, cur, count + 1, res)
-            if count > 0:
-                self.backtrack(word, pos + 1, cur + str(count) + word[pos], 0, res)
-            else:
-                self.backtrack(word, pos + 1, cur + word[pos], 0, res)
+            self.backtrack(word, pos + 1, cur + str_count + word[pos], 0, res)
