7a3391437b677808f6345614e37cf61f5149552a
zhucebuliaolongchuan <cl4076@nyu.edu>
2017-04-18 14:00:19 -0400

    updated on 2017-04-18 14:00:18

---
+++ HashTable/LC454_4Sum2.py
@@ -0,0 +1,35 @@
+"""
+454. 4Sum 2
+Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+Example:
+Input:
+A = [ 1, 2]
+B = [-2,-1]
+C = [-1, 2]
+D = [ 0, 2]
+Output:
+2
+Explanation:
+The two tuples are:
+1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+"""
+class Solution(object):
+    def fourSumCount(self, A, B, C, D):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :type C: List[int]
+        :type D: List[int]
+        :rtype: int
+        """
+        d = collections.defaultdict(int)
+        for i in range(len(C)):
+            for j in range(len(D)):
+                d[C[i] + D[j]] += 1
+        res = 0
+        for p in range(len(A)):
+            for q in range(len(B)):
+                res += d[(A[p] + B[q]) * (-1)]
+        return res
